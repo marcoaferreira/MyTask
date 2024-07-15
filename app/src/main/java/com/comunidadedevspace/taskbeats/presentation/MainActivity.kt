6499214d97a53554f4288d593428b29f903fff90
marco <marcoaferreira@gmail.com>
2024-07-15 15:38:01 -0300

    [patch] Added Fragments List/News

--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
 import com.comunidadedevspace.taskbeats.R
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,31 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+
+        val taskListFragment = TaskListFragment.newInstance()
+        val newsListFragment = NewsListFragment.newInstance()
+
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view, taskListFragment)
+            setReorderingAllowed(true)
+        }
+
+        bottomNavView.setOnItemSelectedListener {menuItem ->
+            val fragment = when (menuItem.itemId){
+                R.id.task_list -> taskListFragment
+                R.id.news_list -> newsListFragment
+                else -> taskListFragment
+            }
+
+            supportFragmentManager.commit {
+                replace(R.id.fragment_container_view, fragment)
+                setReorderingAllowed(true)
+            }
+
+            true
         }
     }
 }
